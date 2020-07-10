a68d17c33aabe52df991e10f30632dbd0284d511
Michael Sanchez <michaelsanchez@pingidentity.com>
2020-07-10 01:24:32 +0000

    reorganized object creation and added 2 new components for OAuth and OpenBanking. Changed array variable name and updated cnosole.log test.

--- bxfinance-scenario1/src/pages/accounts-dashboard.js
+++ bxfinance-scenario1/src/pages/accounts-dashboard.js
@@ -12,6 +12,8 @@ import CardRewards from '../components/CardRewards';
 import Session from '../components/Utils/Session'; /* PING INTEGRATION */
 import PingData from '../components/Utils/PingData'; /* PING INTEGRATION */
 import JSONSearch from '../components/Utils/JSONSearch'; /* PING INTEGRATION */
+import PingOAuth from '../components/Utils/PingOAuth'; /* PING INTEGRATION: */
+import OpenBanking from '../components/Utils/OpenBanking'; /* PING INTEGRATION: */
 
 // Data
 import data from '../data/accounts-dashboard.json';
@@ -22,17 +24,33 @@ import "../styles/pages/accounts.scss";
 class AccountsDashboard extends React.Component {
   constructor() {
     super();
-    this.Session = new Session(); /* PING INTEGRATION */
-    this.PingData = new PingData(); /* PING INTEGRATION */
-    this.JSONSearch = new JSONSearch(); /* PING INTEGRATION: */
+    /* BEGIN PING INTEGRATION: */
+    this.Session = new Session();
+    this.PingData = new PingData();
+    this.JSONSearch = new JSONSearch();
+    this.PingOAuth = new PingOAuth(); 
+    this.OpenBanking = new OpenBanking();
+    /* END PING INTEGRATION: */
   }
 
   /* BEGIN PING INTEGRATION: */
   componentDidMount() {
-    let tmpArr = [];
+    let acctIDsArr = [];
+
     this.PingData.getUserEntry(this.Session.getAuthenticatedUserItem("uid"))
     .then(response => response.json())
-      .then(jsonData => console.log("test:", this.JSONSearch.findValuesHelper(jsonData, "bxFinanceUserAccountIDs")))
+    .then(jsonData => {
+      acctIDsArr = this.JSONSearch.findValues(jsonData, "bxFinanceUserAccountIDs");
+      if (acctIDsArr.length) {
+        //Store their account IDs in their browser session. (as a string).
+        this.Session.setAuthenticatedUserItem("accounts", acctIDsArr.join());
+        
+
+      } else {
+        // provision accts and update array
+      }
+    })
+      //.then(jsonData => console.log("test:", this.JSONSearch.findValues(jsonData, "bxFinanceUserAccountIDs")))
     //.then(jsonData => console.log("test:", JSON.stringify(jsonData)))
     /* .then(jsonData => {
       tmpArr = this.JSONSearch.getValues(jsonData, "bxFinanceUserAccountIDs");
