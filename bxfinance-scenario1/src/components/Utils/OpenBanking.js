20852c2716d2f5151c9e08dc507d7ed6f9e679c0
Michael Sanchez <michaelsanchez@pingidentity.com>
2020-09-04 03:31:04 +0000

    Minor changes to log entries, layout

--- bxfinance-scenario1/src/components/Utils/OpenBanking.js
+++ bxfinance-scenario1/src/components/Utils/OpenBanking.js
@@ -13,13 +13,13 @@ import JSONSearch from './JSONSearch';
 import PingData from './PingData';
 
 export default class OpenBanking {
-    
+
     constructor() {
         // Didn't abstract these since they shouldn't ever change.
         //TODO move these out of the constructor. Not sure why I did this. Not necessary. Didn't do it anywhere else. That's what I get for working on a Saturday.
         this.mockOBConsenthost = process.env.REACT_APP_HOST + "/OpenBanking";
         // this.mockOBhost = "https://demo-openbanking-api-server.herokuapp.com/OpenBanking";
-        this.mockOBhost="https://babbtx-aspsp.herokuapp.com/OpenBanking";
+        this.mockOBhost = "https://babbtx-aspsp.herokuapp.com/OpenBanking";
         this.mockOBAPIver = "/v2";
         this.mockOBbalURI = "/balances";
         this.mockOBacctsURI = "/accounts";
@@ -27,10 +27,13 @@ export default class OpenBanking {
         this.PingData = new PingData();
         this.xfrMoneyURI = "/transferMoney?amount=";
     }
-    
+
 
     /* 
       Provision Banking Accounts
+      Provisions new accounts and balances and updates the user entry in PD.
+      Design pattern debate: Whether to just return the accounts response (strict single responsiblilty), or
+      or as is now, fulfills all tasks of "provisioning an acct", which should include upating the user entry.
       @param token the access token for the authenticated user.
       @response object
       */
@@ -45,17 +48,17 @@ export default class OpenBanking {
             redirect: 'follow'
         };
         const url = this.mockOBhost + this.mockOBAPIver + this.mockOBacctsURI;
-        fetch(url , requestOptions)
+        fetch(url, requestOptions)
             .then(response => response.json())
             .then(jsonData => {
-                // console.log("jsonData:", jsonData);
+                console.log("jsonData:", JSON.stringify(jsonData));
                 acctIdsArr = this.JSONSearch.findValues(jsonData, "AccountId");
-                // console.log("acctIdsArr:", acctIdsArr);
+                console.log("acctIdsArr:", acctIdsArr);
                 this.PingData.updateUserEntry(acctIdsArr, uid);
             })
             .catch(error => console.log('error', error));
-            //TODO try catch error handling
-            return true;
+        //TODO try catch error handling to return true or false.
+        return true;
     }
 
     /* 
@@ -87,21 +90,21 @@ export default class OpenBanking {
     @return boolean stating success
     */
     transferMoney(amount, token) {
-       console.log("transferMoney", arguments);
+        console.log("transferMoney", arguments);
         console.log("token", token);
 
-       let myHeaders = new Headers();
-       myHeaders.append("Authorization", "Bearer " + token);
+        let myHeaders = new Headers();
+        myHeaders.append("Authorization", "Bearer " + token);
 
-       const requestOptions = {
-           method: 'GET',
-           headers: myHeaders,
-           redirect: 'follow'
-       };
-       const url = process.env.REACT_APP_HOST + this.xfrMoneyURI + amount;
-       return fetch(url, requestOptions);
-           /* .then(response => response.text())
-           .then(result => console.log("XFR Response",result))
-           .catch(error => console.log('error', error)); */
-   }
+        const requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+        const url = process.env.REACT_APP_HOST + this.xfrMoneyURI + amount;
+        return fetch(url, requestOptions);
+        /* .then(response => response.text())
+        .then(result => console.log("XFR Response",result))
+        .catch(error => console.log('error', error)); */
+    }
 }
